a77a3868e94bd67ba415bd01b5bcab3d9d1d6e78
Tolu Adeboye <mailtoluwa@gmail.com>
2026-04-11 01:39:29 +0100

    Made code updates and comments in the transaction file

--- transaction.py
+++ transaction.py
@@ -28,20 +28,17 @@ class Transaction:
             raise InvalidTransactionError("Invalid date format. Use YYYY-MM-DD.")
 
         # TODO: Validate amount > 0.
-        # Validating the amount
         if amount <= 0:
             raise InvalidTransactionError("Amount must be greater than zero.")
 
         # TODO: Validate trans_type is "income" or "expense".
-        # validating the transaction type
         if trans_type not in self.VALID_TYPES:
             raise InvalidTransactionError(
                 "Invalid transaction type. Must be an 'income' or 'expense'."
             )
 
         # TODO: Validate category and description are non-empty.
-        # Validating the category and description
-        # Using the strip() method to clean the 'string' attributes.
+        # Using the strip() method to clean the 'string' attributes. So users cannot enter only spaces as category or description.
 
         if not category.strip():
             raise InvalidTransactionError("Category cannot be empty.")
@@ -52,7 +49,9 @@ class Transaction:
 
         # TODO: Assign all attributes.
         self.date = date
-        self.amount = amount
+        self.amount = float(
+            amount
+        )  # Float because we want to allow decimal values for amounts, such as 19.99.
         self.category = category.strip()
         self.description = description.strip()
         self.trans_type = trans_type
@@ -61,22 +60,19 @@ class Transaction:
         """Return True if this is an expense."""
         # TODO: Check self.trans_type
         return self.trans_type == "expense"
-        # pass
-        # No need to define is_income() since there can only be two types of transactions.
+        # We aren't defining is_income() because we can just check if not is_expense() to determine if it's an income.
 
     def get_month(self):
         """Return the month portion as YYYY-MM."""
         # TODO: Return self.date[:7]
         # returns the first seven characters of the date string - YYYY-MM.
         return self.date[:7]
-        # pass
 
     def __str__(self):
         """Returning a formatted string for display."""
         # TODO: Use f-string with aligned columns.
         # This returns the f-string in columns, separated by Tabs(\t).
         return f"{self.date}\t{self.amount}\t{self.category}\t{self.description}"
-        # pass
 
     def to_dict(self):
         """Converting to dictionary for JSON serialisation."""
@@ -89,7 +85,6 @@ class Transaction:
             "description": self.description,
             "trans_type": self.trans_type,
         }
-        # pass
 
     @classmethod
     def from_dict(cls, data):
